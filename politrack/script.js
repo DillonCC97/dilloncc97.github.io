11f2c8fc803f4942beb58d87b036d6006e8c5b87
dilloncc97 <dillonculp51@gmail.com>
2017-03-15 14:09:55 -0400

    finish preliminary frontend of politrack

--- politrack/script.js
+++ politrack/script.js
@@ -4,3 +4,24 @@ function toggleDropdown() {
 }
 var dropdown = document.querySelector("#adv-options");
 dropdown.addEventListener("click", toggleDropdown);
+var ors_menu = document.querySelector("#ors_menu");
+var ors_button = document.querySelector("#ors");
+var ure_button = document.querySelector("#ure");
+var ure_menu = document.querySelector("#ure_menu");
+
+function showOrsMenu() {
+    console.log("Show Legislators Menu");
+    ure_menu.style.display = "none";
+    ors_menu.style.display = "block";
+    document.querySelector(".form-control").placeholder = "Search for Legislator by Name";
+}
+
+function showUreMenu() {
+    console.log("Show Legislature Menu");
+    ors_menu.style.display = "none";
+    ure_menu.style.display = "block";
+    document.querySelector(".form-control").placeholder = "Search for Legislation or Bill ID";
+}
+showOrsMenu();
+ors_button.addEventListener("click", showOrsMenu);
+ure_button.addEventListener("click", showUreMenu);
